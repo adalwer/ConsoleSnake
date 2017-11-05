0bcbe9cff66960898a07a487cc10c553d2ef2767
Adam Furtak <adam.furtak@outlook.com>
2017-11-05 12:53:55 +0100

    initial project commit

---
+++ cmake-build-debug/CMakeFiles/ConsoleSnake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ConsoleSnake.dir/main.cpp.o"
+  "ConsoleSnake.pdb"
+  "ConsoleSnake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ConsoleSnake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,6 @@
+k#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
